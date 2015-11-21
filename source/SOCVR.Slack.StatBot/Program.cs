fa4402119c75428b1b7b9ed1bd0d9702900e831c
gunr2171 <kevin@kevin-hunter.net>
2015-11-20 23:44:33 -0500

    trying out another method for signals

--- source/SOCVR.Slack.StatBot/Program.cs
+++ source/SOCVR.Slack.StatBot/Program.cs
@@ -12,10 +12,13 @@ namespace SOCVR.Slack.StatBot
 {
     class Program
     {
+        static Bot bot = new Bot();
+        static bool gotExitCommand = false;
+
         static void Main(string[] args)
         {
             var botAPIKey = SettingsAccessor.GetSetting<string>("SlackBotAPIKey");
-            var bot = new Bot();
+
             bot.Aliases = new List<string>() { "sc" };
             bot.Responders.Add(new Responders.StatsReponder());
             bot.Responders.Add(new Responders.HelpResponder());
@@ -27,8 +30,59 @@ namespace SOCVR.Slack.StatBot
                 Console.WriteLine("Got signal to shut down.");
             };
 
-            while (true) { }
+#if MONO
+            var unixSignalManager = new UnixExitSignal();
+            unixSignalManager.Exit += UnixSignalManager_Exit;
+#endif
+
+            while (!gotExitCommand) { }
+        }
+
+#if MONO
+        private static void UnixSignalManager_Exit(object sender, EventArgs e)
+        {
+            bot.Disconnect();
+            Console.WriteLine("Got exit command from linux.");
+            gotExitCommand = true;
+        }
+#endif
+    }
+
+#if MONO
+    //http://stackoverflow.com/a/32716784/1043380
+    //no idea if this is going to work
+
+    public interface IExitSignal
+    {
+        event EventHandler Exit;
+    }
+
+    public class UnixExitSignal : IExitSignal
+    {
+        public event EventHandler Exit;
+
+        UnixSignal[] signals = new UnixSignal[]{
+        new UnixSignal(Mono.Unix.Native.Signum.SIGTERM),
+        new UnixSignal(Mono.Unix.Native.Signum.SIGINT),
+        new UnixSignal(Mono.Unix.Native.Signum.SIGUSR1)
+    };
+
+        public UnixExitSignal()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                // blocking call to wait for any kill signal
+                int index = UnixSignal.WaitAny(signals, -1);
+
+                if (Exit != null)
+                {
+                    Exit(null, EventArgs.Empty);
+                }
+
+            });
         }
 
     }
+#endif
+
 }
