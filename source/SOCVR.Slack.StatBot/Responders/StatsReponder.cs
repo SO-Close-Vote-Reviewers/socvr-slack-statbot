3dcc52328ba3c0d1df110a202cb1cfbbcdbda21b
gunr2171 <kevin@kevin-hunter.net>
2015-11-20 21:18:35 -0500

    updating the regex in the stats responder

--- source/SOCVR.Slack.StatBot/Responders/StatsReponder.cs
+++ source/SOCVR.Slack.StatBot/Responders/StatsReponder.cs
@@ -21,7 +21,7 @@ namespace SOCVR.Slack.StatBot.Responders
                                            |           |     |                                                               the date the user wants data for. Can type in an ISO format,
                                            |           |     |                                                               or you can use "today", "yesterday", or "X days ago"
                                            |           |     |                                                               |                                               The time range for the messages. Optional.*/
-        Regex commandPattern = new Regex(@"([\w-]+?):? stats (?:(totals|cv-pls|links|moved|one-box|stars|starred|stars-in) )?((\d{4})-(\d{2})-(\d{2})|today|yesterday|(\d+) days ago)(?: (\d{1,2})-(\d{1,2}))?", RegexOptions.IgnoreCase);
+        Regex commandPattern = new Regex(@"stats (?:(totals|cv-pls|links|moved|one-box|stars|starred|stars-in) )?((\d{4})-(\d{2})-(\d{2})|today|yesterday|(\d+) days ago)(?: (\d{1,2})-(\d{1,2}))?", RegexOptions.IgnoreCase);
 
         ChatScraper cs = new ChatScraper();
 
@@ -37,9 +37,9 @@ namespace SOCVR.Slack.StatBot.Responders
         {
             var match = commandPattern.Match(context.Message.Text.ToLower());
 
-            var filter = match.Groups[2].Value;
-            var startHourRaw = match.Groups[8].Value;
-            var endHourRaw = match.Groups[9].Value;
+            var filter = match.Groups[1].Value;
+            var startHourRaw = match.Groups[7].Value;
+            var endHourRaw = match.Groups[8].Value;
 
             DateTime date = ExtractDateFromMatch(match);
 
@@ -102,26 +102,6 @@ namespace SOCVR.Slack.StatBot.Responders
             }
         }
 
-        private string GetStarData(List<ChatMessageInfo> messages)
-        {
-            var data = messages
-                .GroupBy(x => x.UserName)
-                .Select(x => new
-                {
-                    UserName = x.Key,
-                    StarredMessages = x.Count(m => m.StarCount > 0),
-                    TotalStars = x.Sum(m => m.StarCount)
-                })
-                .Where(x => x.TotalStars > 0)
-                .OrderByDescending(x => x.TotalStars)
-                .ToStringTable(new[] { "Username", "Total Stars", "# Starred Msgs" },
-                    (x) => x.UserName,
-                    (x) => x.TotalStars,
-                    (x) => x.StarredMessages);
-
-            return "```" + data + "```";
-        }
-
         private DateTime ExtractDateFromMatch(Match match)
         {
             /* the date is in one of the following formats:
@@ -131,27 +111,27 @@ namespace SOCVR.Slack.StatBot.Responders
              * relative - "X days ago"
              */
 
-            //if group 4 (direct year) is used then it's in the direct format
-            if (match.Groups[4].Success)
+            //if group 3 (direct year) is used then it's in the direct format
+            if (match.Groups[3].Success)
             {
-                var year = match.Groups[4].Value.Parse<int>();
-                var month = match.Groups[5].Value.Parse<int>();
-                var day = match.Groups[6].Value.Parse<int>();
+                var year = match.Groups[3].Value.Parse<int>();
+                var month = match.Groups[4].Value.Parse<int>();
+                var day = match.Groups[5].Value.Parse<int>();
                 return new DateTime(year, month, day);
             }
-            //else, if group 7 (X days ago) is used then it's relative format
-            else if (match.Groups[7].Success)
+            //else, if group 6 (X days ago) is used then it's relative format
+            else if (match.Groups[6].Success)
             {
                 var daysAgo = match.Groups[7].Value.Parse<int>();
                 return DateTime.UtcNow.Date.AddDays(-daysAgo);
             }
-            //else, it's either "today" or "yesterday" in group 3. Check "today" first
-            else if (match.Groups[3].Value == "today")
+            //else, it's either "today" or "yesterday" in group 2. Check "today" first
+            else if (match.Groups[2].Value == "today")
             {
                 return DateTime.UtcNow.Date;
             }
             //else, try "yesterday"
-            else if (match.Groups[3].Value == "yesterday")
+            else if (match.Groups[2].Value == "yesterday")
             {
                 return DateTime.UtcNow.Date.AddDays(-1);
             }
