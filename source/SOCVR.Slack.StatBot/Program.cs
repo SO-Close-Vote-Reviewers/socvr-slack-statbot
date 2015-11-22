46c353af9023c8488430709591f4d1ee8c9ce0c1
gunr2171 <kevin@kevin-hunter.net>
2015-11-21 23:11:59 -0500

    I can't get the Unix stuff to work, removing it for now later I'll try to get it working with sigterm again

--- source/SOCVR.Slack.StatBot/Program.cs
+++ source/SOCVR.Slack.StatBot/Program.cs
@@ -30,59 +30,9 @@ namespace SOCVR.Slack.StatBot
                 Console.WriteLine("Got signal to shut down.");
             };
 
-#if MONO
-            var unixSignalManager = new UnixExitSignal();
-            unixSignalManager.Exit += UnixSignalManager_Exit;
-#endif
-
             while (!gotExitCommand) { }
         }
 
-#if MONO
-        private static void UnixSignalManager_Exit(object sender, EventArgs e)
-        {
-            bot.Disconnect();
-            Console.WriteLine("Got exit command from linux.");
-            gotExitCommand = true;
-        }
-#endif
     }
-
-#if MONO
-    //http://stackoverflow.com/a/32716784/1043380
-    //no idea if this is going to work
-
-    public interface IExitSignal
-    {
-        event EventHandler Exit;
-    }
-
-    public class UnixExitSignal : IExitSignal
-    {
-        public event EventHandler Exit;
-
-        UnixSignal[] signals = new UnixSignal[]{
-        new UnixSignal(Mono.Unix.Native.Signum.SIGTERM),
-        new UnixSignal(Mono.Unix.Native.Signum.SIGINT),
-        new UnixSignal(Mono.Unix.Native.Signum.SIGUSR1)
-    };
-
-        public UnixExitSignal()
-        {
-            Task.Factory.StartNew(() =>
-            {
-                // blocking call to wait for any kill signal
-                int index = UnixSignal.WaitAny(signals, -1);
-
-                if (Exit != null)
-                {
-                    Exit(null, EventArgs.Empty);
-                }
-
-            });
-        }
-
-    }
-#endif
 
 }
