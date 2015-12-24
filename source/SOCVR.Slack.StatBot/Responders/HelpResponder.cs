64f29c22f6f4827c49f44a367ee343ca8912b431
gunr2171 <kevin@kevin-hunter.net>
2015-12-23 22:42:27 -0500

    updating the help text

--- source/SOCVR.Slack.StatBot/Responders/HelpResponder.cs
+++ source/SOCVR.Slack.StatBot/Responders/HelpResponder.cs
@@ -18,10 +18,12 @@ namespace SOCVR.Slack.StatBot.Responders
             return new BotMessage()
             {
                 Text = @"SOCVR Stat Slackbot, developed by gunr2171. Usage is as follows:
-```sc stats [filter] <date> [hourStart-hourEnd]```
-`Filter` is optional. Leave out to show the summary table, or choose from one of the following: totals, cv-pls, links, moved, one-box, stars
+```sc day-stats [filter] <date> [hourStart-hourEnd] [output-type]
+sc range-stats [filter] <start date> <end date> [output-type]```
+`Filter` is optional. Leave out to show the summary table, or choose from one of the following: totals, cv-pls, links, moved, one-box, stars.
 `Date` can be an ISO format date (`yyyy-MM-dd`), 'today', 'yesterday', or 'X days ago'.
-The hour can be specified, use numbers 0-24."
+The hour can be specified, use numbers 0-24.
+`Output Type` can be specified, options are 'summary-only' and 'table'. Leaving this blank will choose the first option."
             };
         }
     }
