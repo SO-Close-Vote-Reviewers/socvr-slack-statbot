5f230d21f0e3ed35758ba7c30868bc340f6c8110
gunr2171 <kevin@kevin-hunter.net>
2016-01-24 13:34:04 -0500

    adding in code to do the downloading

--- source/SOCVR.Slack.StatBot/Parsing/Url/RateLimitedUrl.cs
+++ source/SOCVR.Slack.StatBot/Parsing/Url/RateLimitedUrl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -32,8 +33,10 @@ namespace SOCVR.Slack.StatBot.Parsing.Url
                     Thread.Sleep((int)milisecondsToWait);
                 }
 
-                //download the html
-                throw new NotImplementedException();
+                using (var htmlClient = new WebClient())
+                {
+                    return htmlClient.DownloadString(Url);
+                }
             }
         }
 
